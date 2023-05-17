c8f7a749121c079e07bc6fdda9eb4ec3b10e3b61
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-17 16:49:12 -0300

    feat: config styled components and add font Inter

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  'gray-900': '#1E1E1E',
+  'gray-800': '#2B2B2B',
+  'gray-400': '#898888',
+  'gray-100': '#DBDBDB',
+
+  'green-500': '#00875F',
+  'green-700': '#035C42',
+} as const;
