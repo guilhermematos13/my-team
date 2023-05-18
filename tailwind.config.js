f3f6d9ab423f1c20fdfa9a75ca77e56e3f22190a
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-18 12:13:49 -0300

    feat: config tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
