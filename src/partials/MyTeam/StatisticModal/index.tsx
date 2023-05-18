1f94f9d9e442d1d5ef1aaba1fe0313795ef351fa
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-18 20:57:12 -0300

    feat: created statistics modal

--- src/partials/MyTeam/StatisticModal/index.tsx
+++ src/partials/MyTeam/StatisticModal/index.tsx
@@ -1,9 +1,25 @@
 import { Modal } from '../../../components/Modal';
+import { Content, Header } from './styles';
 
 export function StatisticModal() {
   return (
     <Modal title="Estatisticas">
-      <div>Estatisticas</div>
+      <Content>
+        <strong>Total de jogos</strong>
+        <p>38</p>
+      </Content>
+      <Content>
+        <strong>Vitórias</strong>
+        <p>16</p>
+      </Content>
+      <Content>
+        <strong>Empates</strong>
+        <p>6</p>
+      </Content>
+      <Content>
+        <strong>Derrotas</strong>
+        <p>14</p>
+      </Content>
     </Modal>
   );
 }
---
+++ src/partials/MyTeam/StatisticModal/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  display: flex;
+  margin-top: 8px;
+  justify-content: space-between;
+  padding: 16px 40px;
+  background-color: ${(props) => props.theme['gray-900']};
+  align-items: center;
+
+  p {
+    font-size: 20px;
+    color: ${(props) => props.theme['gray-100']};
+  }
+
+  strong {
+    font-size: 20px;
+    color: ${(props) => props.theme['gray-100']};
+    font-weight: bold;
+  }
+`;
