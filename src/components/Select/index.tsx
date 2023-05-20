9e4ebc55c718cdde5e5b2979f5d29c7bf3829adb
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-19 21:09:13 -0300

    refactor: change select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,67 +1,77 @@
-import * as Select from '@radix-ui/react-select';
+import clsx from 'clsx';
 import { Container, SelectTitle } from './styles';
-import { CaretDown, Check } from '@phosphor-icons/react';
-import { forwardRef } from 'react';
+import { ForwardedRef, SelectHTMLAttributes, forwardRef } from 'react';
+import Select from 'react-select';
 
-interface SelectFormProps {
+interface SelectFormProps extends SelectHTMLAttributes<HTMLSelectElement> {
   placeholder: string;
   title: string;
+  options: { value: string; label: string }[];
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const SelectItem = forwardRef(({ children, ...props }: any, forwardRef) => {
-  return (
-    <Select.Item {...props} ref={forwardRef}>
-      <Select.ItemText>{children}</Select.ItemText>
-      <Select.ItemIndicator>
-        <Check size={20} />
-      </Select.ItemIndicator>
-    </Select.Item>
-  );
-});
+const controlStyles = {
+  base: 'rounded-lg bg-[#1E1E1E] hover:cursor-pointer p-4 text-[#DBDBDB]',
+  focus: 'border border-[#00875F]',
+  nonFocus: 'border-[#00875F]',
+};
+const placeholderStyles = 'text-[#898888] pl-1 py-0.5';
+const selectInputStyles = 'pl-1 py-0.5';
+const valueContainerStyles = 'p-1 gap-1';
+const singleValueStyles = 'leading-7 ml-1';
+const indicatorsContainerStyles = 'p-1 gap-3';
+const indicatorSeparatorStyles = 'bg-[#00875F]';
+const dropdownIndicatorStyles =
+  'p-1 hover:bg-[#00875F] text-gray-500 rounded-md hover:text-[#DBDBDB]';
+const menuStyles = 'p-2 mt-1 border border-gray-200 bg-white rounded-lg';
+const optionStyles = {
+  base: 'hover:cursor-pointer px-3 py-2 rounded hover:bg-[#D7F9EF]',
+  focus: 'bg-gray-100 active:bg-[#D7F9EF] bg-[#D7F9EF]',
+  selected:
+    "after:content-['✔'] after:ml-2 after:text-green-500 text-gray-500 ",
+};
+const noOptionsMessageStyles =
+  'text-gray-500 p-2 bg-gray-50 border border-dashed border-gray-200 rounded-sm';
 
-export function SelectForm({ placeholder, title }: SelectFormProps) {
-  return (
-    <Container>
-      <SelectTitle>{title}</SelectTitle>
-      <Select.Root>
-        <Select.Trigger
-          aria-label="country"
-          className="inline-flex items-center justify-between rounded-lg p-4 leading-[1] outline-none w-[590px] bg-[#1E1E1E] text-[#DBDBDB]"
-        >
-          <Select.Value placeholder={placeholder} />
-          <Select.Icon>
-            <CaretDown size={32} />
-          </Select.Icon>
-        </Select.Trigger>
-        <Select.Content
-          position="popper"
-          className="bg-[#1E1E1E] p-4 text-base w-[590px] max-h-[500px] -mt-1 rounded-b-lg"
-        >
-          <Select.Viewport className="p-1">
-            <Select.Group>
-              <SelectItem
-                value="Brasil"
-                className="flex justify-between p-4 text-[#DBDBDB]"
-              >
-                Brasil
-              </SelectItem>
-              <SelectItem
-                value="Argentina"
-                className="flex justify-between p-4 text-[#DBDBDB]"
-              >
-                Argentina
-              </SelectItem>
-              <SelectItem
-                value="Inglaterra"
-                className="flex justify-between p-4 text-[#DBDBDB]"
-              >
-                Inglaterra
-              </SelectItem>
-            </Select.Group>
-          </Select.Viewport>
-        </Select.Content>
-      </Select.Root>
-    </Container>
-  );
-}
+export const SelectForm = forwardRef(
+  (
+    { placeholder, title, options }: SelectFormProps,
+    ref: ForwardedRef<never>
+  ) => {
+    return (
+      <Container>
+        <SelectTitle>{title}</SelectTitle>
+        <Select
+          ref={ref}
+          placeholder={placeholder}
+          options={options}
+          noOptionsMessage={({ inputValue }) =>
+            !inputValue && 'Nenhum resultado'
+          }
+          unstyled
+          classNames={{
+            control: ({ isFocused }) =>
+              clsx(
+                isFocused ? controlStyles.focus : controlStyles.nonFocus,
+                controlStyles.base
+              ),
+            placeholder: () => placeholderStyles,
+            input: () => selectInputStyles,
+            valueContainer: () => valueContainerStyles,
+            singleValue: () => singleValueStyles,
+            indicatorsContainer: () => indicatorsContainerStyles,
+            indicatorSeparator: () => indicatorSeparatorStyles,
+            dropdownIndicator: () => dropdownIndicatorStyles,
+            menu: () => menuStyles,
+            option: ({ isFocused, isSelected }) =>
+              clsx(
+                isFocused && optionStyles.focus,
+                isSelected && optionStyles.selected,
+                optionStyles.base
+              ),
+            noOptionsMessage: () => noOptionsMessageStyles,
+          }}
+        />
+      </Container>
+    );
+  }
+);
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -1,7 +1,7 @@
-import * as Select from '@radix-ui/react-select';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 590px;
   display: flex;
   flex-direction: column;
   gap: 12px;
@@ -10,23 +10,4 @@ export const Container = styled.div`
 export const SelectTitle = styled.label`
   font-size: 20px;
   color: ${(props) => props.theme['gray-100']};
-`;
-
-export const SelectStyled = styled(Select.Trigger)`
-  width: 590px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  border: 0;
-  border-radius: 8px;
-  font-size: 16px;
-  outline: none;
-
-  background: ${(props) => props.theme['gray-900']};
-  color: ${(props) => props.theme['gray-400']};
-
-  :focus {
-    border: 1px solid ${(props) => props.theme['green-500']};
-  }
 `;
